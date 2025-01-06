98e0b47c45b7f66e5adefc0c77710a198f5db920
KrOySi <67242784+KrOySi@users.noreply.github.com>
2025-01-06 12:09:32 +0300

    1

---
+++ js/config.js
@@ -0,0 +1,44 @@
+let config = {
+
+  // Задержка загрузки (1 секунда)
+  preloaderDelay: 1,
+
+  // Задержка появления видео (4.2 секунды)
+  showDelay: 1.5, 
+
+  // Заголовок
+  title: 'av3lon',
+
+  // Социальные сети
+  social: {
+    telegram_kroysi: '@av3lon',
+    vk_kroysi: 'av3lone',
+  }, 
+
+
+  // Цвет иконки загрузки
+  preloaderColor: '#1E90FF',
+
+
+  // Цвет кнопки play
+  playButtonColor: '#00BFFF',
+  // Цвет тени кнопки play
+  playButtonShadowColor: 'rgba(255, 255, 255, 0.5)',
+
+
+  // Цвет заголовка
+  titleColor: '#00BFFF',
+  // Цвет тени загаловка
+  titleShadowColor: 'rgba(255, 255, 255, 0.5)',
+
+
+  // Текст копирайта
+  copyrightText: 'get skeet.',
+  // Цвет копирайта
+  copyrightTextColor: '#0000CD',
+
+  // Цвет иконок социальных сетей
+  socialColor: '#00BFFF',
+  // Цвет тени иконок социальных сетей
+  socialShadowColor: 'rgba(255, 255, 255, 0.5)',
+}
